7e902fadc1e49a92f62ad465b37269ff7bb182cd
Romis <romis@nesmelov.com>
2023-11-26 18:35:44 +0300

    continue making layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
         <div id="head-container">
             <h1>{{ trans('content.main_image_text') }}</h1>
         </div>
-        <div id="arch"></div>
+        <img id="arch" src="{{ asset('images/arch.svg') }}" />
         <img class="arrow-down" src="{{ asset('images/arrow_cir_to_down_yellow.svg') }}">
     </div>
     <div id="content">
