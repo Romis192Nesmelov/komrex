ef8e673b23813740a8eb03fb333e8349bfb02c4c
Romis <romis@nesmelov.com>
2024-01-18 07:13:06 +0300

    added yandex-metrika

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -47,6 +47,24 @@
 </head>
 
 <body>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(96163658, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/96163658" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+
     @yield('content')
     <div class="footer wow animate__animated animate__fadeIn" data-scroll-destination="contacts">
         <div>
@@ -107,9 +125,7 @@
         </form>
     </x-smodal>
 
-    <x-smodal id="personal-data-modal" head="{{ trans('content.transfer_and_processing_of_personal_data') }}">
-
-    </x-smodal>
+{{--    <x-smodal id="personal-data-modal" head="{{ trans('content.transfer_and_processing_of_personal_data') }}"></x-smodal>--}}
 
     @if ($cookieInfo)
         <x-smodal id="cookie-info-modal" head="{{ trans('content.welcome_to_komreks') }}">
